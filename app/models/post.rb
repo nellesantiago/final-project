0a5501e4f1a3e8543a6a3f7fb46948e54010f6a8
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-07-31 19:30:56 +0800

    add like feature to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
-
   has_one_attached :content_image
+
+  has_many :likes
+  has_many :users_liked, through: :likes, source: :user
+
+  def liked?(user)
+    !!self.likes.find_by(user_id: user.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_31_084037) do
+ActiveRecord::Schema.define(version: 2022_07_31_111612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 2022_07_31_084037) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.integer "price"
@@ -85,5 +94,7 @@ ActiveRecord::Schema.define(version: 2022_07_31_084037) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
