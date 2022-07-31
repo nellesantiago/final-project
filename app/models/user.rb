e1c2e218575fcb56f5605b6eb7a6b021452b91ff
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-07-31 11:29:22 +0800

    add follow feature to application

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,16 @@ class User < ApplicationRecord
   enum role: {fan: 0, creator: 1, admin: 2}
 
   has_many :posts, dependent: :destroy
+
+  has_many :received_follows, foreign_key: :followed_id, class_name: "Follow"
+  has_many :followers, through: :received_follows, source: :follower
+
+  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+  has_many :followings, through: :given_follows, source: :followed_user
+  has_many :following_posts, through: :followings, source: :posts
+
+
+  def followed?(user)
+    !!self.given_follows.find_by(followed_id: user.id)
+  end
 end
