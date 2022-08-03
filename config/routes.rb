45f925ddd9bf4bb7c2a4a7986ca1058d78e8e923
Junelle Santiago <junellesantiago@gmail.com>
2022-08-03 18:38:59 +0800

    show creator's post

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   resources :users, only: %i[index]
   resources :home, only: %i[index]
   resources :posts, except: %i[index]
-  resources :dashboard, only: %i[index]
+  resources :dashboard, only: %i[index show]
   resources :creator, only: %i[index]
 end
