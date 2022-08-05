206b60cc6c8a2a32e50a818c8675c58503e2b8b6
Junelle Santiago <junellesantiago@gmail.com>
2022-08-05 20:19:25 +0800

    add profile picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
         stored_location_for(resource) || dashboard_index_path
     end
+
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+private
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name email password image username])
+        devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name email password current_password image username])
+    end
 end
